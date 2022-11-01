2e5f3973846ec3579b8d890bceea73ce52b1a7cb
zredcay <60413606+zredcay@users.noreply.github.com>
2022-11-01 14:37:52 -0400

    Async Example

---
+++ AsyncExampleWPF/AsyncExampleWPF/MainWindow.xaml.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Net.Http
+Imports System.Net
+Imports System.IO
+
+Class MainWindow
+    Private Async Sub startButton_Click(sender As Object, e As RoutedEventArgs) Handles startButton.Click
+        ' Disable the button until the operation is complete.
+        startButton.IsEnabled = False
+        resultsTextBox.Clear()
+        Await SumPageSizesAsync()
+        resultsTextBox.Text &= vbCrLf & "Control returned to startButton_Click."
+        ' Reenable the button in case you want to run the operation again.
+        startButton.IsEnabled = True
+    End Sub
+
+    Private Async Function SumPageSizesAsync() As Task
+        ' Declare an HttpClient object and increase the buffer size. The
+        ' default buffer size is 65,536.
+        Dim client As HttpClient =
+            New HttpClient() With {.MaxResponseContentBufferSize = 1000000}
+        ' Make a list of web addresses.
+        Dim urlList As List(Of String) = SetUpURLList()
+
+        Dim total = 0
+        For Each url In urlList
+            ' GetURLContents returns the contents of url as a byte aray.
+            Dim urlContents As Byte() = Await client.GetByteArrayAsync(url)
+
+            DisplayResults(url, urlContents)
+
+            ' Update the total.
+            total += urlContents.Length
+        Next
+
+        ' Display the total count for all of the web addresses.
+        resultsTextBox.Text &= String.Format(vbCrLf & vbCrLf & "Total bytes returned: {0}" & vbCrLf, total)
+    End Function
+
+    Private Function SetUpURLList() As List(Of String)
+        Dim urls = New List(Of String) From
+            {
+                "https://msdn.microsoft.com/library/windows/apps/br211380.aspx",
+                "https://msdn.microsoft.com",
+                "https://msdn.microsoft.com/library/hh290136.aspx",
+                "https://msdn.microsoft.com/library/ee256749.aspx",
+                "https://msdn.microsoft.com/library/hh290138.aspx",
+                "https://msdn.microsoft.com/library/hh290140.aspx",
+                "https://msdn.microsoft.com/library/dd470362.aspx",
+                "https://msdn.microsoft.com/library/aa578028.aspx",
+                "https://msdn.microsoft.com/library/ms404677.aspx",
+                "https://msdn.microsoft.com/library/ff730837.aspx"
+            }
+        Return urls
+    End Function
+
+    Private Sub DisplayResults(url As String, content As Byte())
+
+        ' Display the length of each website. The string format
+        ' is designed to be used with a monospaced font, such as
+        ' Lucida Console or Global Monospace.
+        Dim bytes = content.Length
+
+        ' Strip off the "https://".
+        Dim displayURL = url.Replace("https://", "")
+        resultsTextBox.Text &= String.Format(vbCrLf & "{0,-58} {1,8}", displayURL, bytes)
+    End Sub
+End Class
