d5984ebe53ae6b8ca2b23d6358469b22e83ef590
zredcay <60413606+zredcay@users.noreply.github.com>
2022-11-01 15:28:54 -0400

    Add files via upload

---
+++ InterfaceWalkthrough/InterfaceWalkthrough/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Dim WithEvents testInstance As TestInterface
+    Sub EventHandler() Handles testInstance.Event1
+        MsgBox("The event handler caught the event.")
+    End Sub
+    Sub Test()
+        '  Create an instance of the class.
+        Dim T As New ImplementationClass
+        ' Assign the class instance to the interface.
+        ' Calls to the interface members are 
+        ' executed through the class instance.
+        testInstance = T
+        ' Set a property.
+        testInstance.Prop1 = 9
+        ' Read the property.
+        MsgBox("Prop1 was set to " & testInstance.Prop1)
+        '  Test the method and raise an event.
+        testInstance.Method1(5)
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object,
+                       ByVal e As System.EventArgs) Handles MyBase.Load
+        Test() ' Test the class.
+    End Sub
+End Class
---
+++ InterfaceWalkthrough/InterfaceWalkthrough/Module1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Runtime.InteropServices
+
+Module Module1
+    Interface TestInterface
+        Property Prop1() As Integer
+        Sub Method1(ByVal X As Integer)
+        Event Event1()
+    End Interface
+    Class ImplementationClass
+        Implements TestInterface
+
+        Private pval As Integer
+
+        Public Property Prop1 As Integer Implements TestInterface.Prop1
+            Get
+                Return pval
+            End Get
+            Set(value As Integer)
+                pval = value
+            End Set
+        End Property
+
+        Public Event Event1() Implements TestInterface.Event1
+
+        Public Sub Method1(X As Integer) Implements TestInterface.Method1
+            MsgBox("The X parameter for Method1 is " & X)
+            RaiseEvent Event1()
+        End Sub
+    End Class
+End Module
