7fdea7b2851d0130ad15a9b088a5eb7b2fe34cdf
zredcay <60413606+zredcay@users.noreply.github.com>
2022-11-02 11:16:31 -0400

    Add files via upload

---
+++ LogFilterWalkthrough/LogFilterWalkthrough/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Activity tracing information
+        My.Application.Log.WriteEntry("Entering Button1_Click", TraceEventType.Start)
+
+        ' Tracing information
+        My.Application.Log.WriteEntry("In Button1_Click", TraceEventType.Information)
+
+        ' Create an exception to log.
+        Dim ex As New ApplicationException
+        ' Exception Information
+        My.Application.Log.WriteException(ex)
+
+        ' Activity tracing information
+        My.Application.Log.WriteEntry("Leaving Button1_Click", TraceEventType.Stop)
+
+        Dim sL As New SimpleListener
+        sL.DisplaySimpleListenerStrongName()
+    End Sub
+End Class
---
+++ LogFilterWalkthrough/LogFilterWalkthrough/SimpleListener.vb
@@ -0,0 +1,17 @@
+﻿Public Class SimpleListener
+    Inherits System.Diagnostics.TraceListener
+
+    <Security.Permissions.HostProtection(Synchronization:=True)>
+    Public Overloads Overrides Sub Write(ByVal message As String)
+        MsgBox("Write: " & message)
+    End Sub
+
+    <Security.Permissions.HostProtection(Synchronization:=True)>
+    Public Overloads Overrides Sub WriteLine(ByVal message As String)
+        MsgBox("WriteLine: " & message)
+    End Sub
+    Public Sub DisplaySimpleListenerStrongName()
+        Dim t As Type = GetType(SimpleListener)
+        MsgBox(t.FullName & ", " & t.Assembly.FullName)
+    End Sub
+End Class
