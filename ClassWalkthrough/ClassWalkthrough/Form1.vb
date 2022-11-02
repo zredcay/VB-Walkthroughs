a6ee94958f0c40123f3c4a54752519f5d41a53f1
zredcay <60413606+zredcay@users.noreply.github.com>
2022-11-02 07:19:33 -0400

    Add files via upload

---
+++ ClassWalkthrough/ClassWalkthrough/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Create an instance of the class.
+        Dim user As New UserNameInfo("Moore, Bobby")
+        ' Capitalize the value of the property.
+        user.Capitalize()
+        ' Display the value of the property.
+        MsgBox("The original UserName is: " & user.UserName)
+        ' Change the value of the property.
+        user.UserName = "Worden, Joe"
+        ' Redisplay the value of the property.
+        MsgBox("The new UserName is: " & user.UserName)
+    End Sub
+End Class
---
+++ ClassWalkthrough/ClassWalkthrough/UserNameInfo.vb
@@ -0,0 +1,21 @@
+﻿Public Class UserNameInfo
+    Private userNameValue As String
+    Public Property UserName() As String
+        Get
+            ' Gets the property value.
+            Return userNameValue
+        End Get
+        Set(value As String)
+            ' Sets the property value.
+            userNameValue = value
+        End Set
+    End Property
+    Public Sub Capitalize()
+        ' Capitalize the value of the property.
+        userNameValue = UCase(userNameValue)
+    End Sub
+    Public Sub New(ByVal UserName As String)
+        ' Set the property value.
+        Me.UserName = UserName
+    End Sub
+End Class
