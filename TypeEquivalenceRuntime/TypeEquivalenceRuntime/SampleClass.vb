6fe6a98cf42044954a4bcbc8c2d6684d7566c137
zredcay <60413606+zredcay@users.noreply.github.com>
2022-11-02 12:26:24 -0400

    Add files via upload

---
+++ TypeEquivalenceRuntime/TypeEquivalenceRuntime/SampleClass.vb
@@ -0,0 +1,20 @@
+﻿Imports TypeEquivalenceInterface
+
+Public Class SampleClass
+    Implements ISampleInterface
+
+    Private p_UserInput As String
+    Public ReadOnly Property UserInput() As String Implements ISampleInterface.UserInput
+        Get
+            Return p_UserInput
+        End Get
+    End Property
+
+    Public Sub GetUserInput() Implements ISampleInterface.GetUserInput
+        Console.WriteLine("Please enter a value:")
+        p_UserInput = Console.ReadLine()
+    End Sub
+    Public Function GetDate() As DateTime Implements ISampleInterface.GetDate
+        Return Now
+    End Function
+End Class
