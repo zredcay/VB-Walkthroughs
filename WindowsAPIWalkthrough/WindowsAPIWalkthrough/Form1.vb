1f19ab71ca69872b10c456dea11611ed7ceeadd5
zredcay <60413606+zredcay@users.noreply.github.com>
2022-11-02 09:09:48 -0400

    Add files via upload

---
+++ WindowsAPIWalkthrough/WindowsAPIWalkthrough/Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Runtime.InteropServices
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ' Stores the return value.
+        Dim RetVal As Integer
+        RetVal = MBox(0, "Declare DLL Test", "Windows API MessageBox", MB_ICONQUESTION Or MB_YESNO)
+
+        ' Check the return value.
+        If RetVal = IDYES Then
+            MsgBox("You chose Yes")
+        Else
+            MsgBox("You chose No")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim RetVal As Boolean = MoveFile(":c\tmp\Test.txt", "c:\Test.txt")
+        If RetVal = True Then
+            MsgBox("The file was moved successfully.")
+        Else
+            MsgBox("The file could not be moved.")
+        End If
+    End Sub
+
+    <DllImport("KERNEL32.DLL", EntryPoint:="MoveFileW", SetLastError:=True,
+    CharSet:=CharSet.Unicode, ExactSpelling:=True,
+    CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function MoveFile(
+    ByVal src As String,
+    ByVal dst As String) As Boolean
+        ' Leave the body of the function empty.
+    End Function
+End Class
+
+
---
+++ WindowsAPIWalkthrough/WindowsAPIWalkthrough/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Runtime.InteropServices
+Module Module1
+    Public Const MB_ICONQUESTION As Integer = &H20
+    Public Const MB_YESNO As Integer = &H4
+    Public Const IDYES As Integer = 6
+    Public Const IDNO As Integer = 7
+
+    Declare Auto Function MBox Lib "user32.dll" Alias "MessageBox" (
+        ByVal hWnd As Integer,
+        ByVal txt As String,
+        ByVal caption As String,
+        ByVal Typ As Integer) As Integer
+    Declare Sub SetData Lib "...\LIB\UnmgdLib.dll" (
+        ByVal x As Short,
+        <MarshalAsAttribute(UnmanagedType.AsAny)>
+        ByVal o As Object)
+End Module
