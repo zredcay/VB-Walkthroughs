565f6fa50eb7ad5a08ce09e49da40f3b3c78337c
zredcay <60413606+zredcay@users.noreply.github.com>
2022-11-02 09:23:12 -0400

    Add files via upload

---
+++ COMWalkthrough/COMWalkthrough/ComClass1.vb
@@ -0,0 +1,11 @@
+﻿<ComClass(ComClass1.ClassId, ComClass1.InterfaceId, ComClass1.EventsId)>
+Public Class ComClass1
+    Public Const ClassId As String = "62860F30-A249-4710-8EC4-C62B6FCF588F"
+    Public Const InterfaceId As String = "6F62713F-E3B2-4594-9B6F-DC74228DA0BB"
+    Public Const EventsId As String = "D3736626-0D63-466E-9CBC-4319F7ED53F1"
+
+    Public Sub New()
+        MyBase.New()
+    End Sub
+
+End Class
---
+++ COMWalkthrough/COMWalkthrough/ComClass2.vb
@@ -0,0 +1,23 @@
+﻿<ComClass(ComClass2.ClassId, ComClass2.InterfaceId, ComClass2.EventsId)> _
+Public Class ComClass2
+
+#Region "COM GUIDs"
+    ' These  GUIDs provide the COM identity for this class 
+    ' and its COM interfaces. If you change them, existing 
+    ' clients will no longer be able to access the class.
+    Public Const ClassId As String = "0451425b-4529-42f1-a202-b6895fe50e6e"
+    Public Const InterfaceId As String = "d78e03e4-4b98-4fff-a567-dae3a6d70790"
+    Public Const EventsId As String = "97661d6c-613c-440c-a36f-59751747f952"
+#End Region
+
+    ' A creatable COM class must have a Public Sub New() 
+    ' with no parameters, otherwise, the class will not be 
+    ' registered in the COM registry and cannot be created 
+    ' via CreateObject.
+    Public Sub New()
+        MyBase.New()
+    End Sub
+
+End Class
+
+
