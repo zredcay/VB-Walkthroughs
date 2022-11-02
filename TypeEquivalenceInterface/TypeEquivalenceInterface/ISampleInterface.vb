0ea38338451cab607064968622f0a165782ae9e9
zredcay <60413606+zredcay@users.noreply.github.com>
2022-11-02 12:26:04 -0400

    Add files via upload

---
+++ TypeEquivalenceInterface/TypeEquivalenceInterface/ISampleInterface.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Runtime.InteropServices
+
+<ComImport()>
+<Guid("16BC804B-DB6D-440B-86BC-6CB4B59B4E87")>
+Public Interface ISampleInterface
+    Sub GetUserInput()
+    ReadOnly Property UserInput As String
+    Function GetDate() As Date
+End Interface
