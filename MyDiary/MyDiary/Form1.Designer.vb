3cfa4c99a7ff48209e7418dff758c248a7390891
zredcay <60413606+zredcay@users.noreply.github.com>
2022-11-02 14:43:50 -0400

    Add files via upload

---
+++ MyDiary/MyDiary/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Submit = New System.Windows.Forms.Button()
+        Me.Clear = New System.Windows.Forms.Button()
+        Me.Entry = New System.Windows.Forms.TextBox()
+        Me.DisplayEntry = New System.Windows.Forms.TextBox()
+        Me.Display = New System.Windows.Forms.Button()
+        Me.GetEntries = New System.Windows.Forms.Button()
+        Me.PickEntries = New System.Windows.Forms.ComboBox()
+        Me.DeleteEntry = New System.Windows.Forms.Button()
+        Me.EditEntry = New System.Windows.Forms.Button()
+        Me.SubmitEdit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Submit
+        '
+        Me.Submit.Location = New System.Drawing.Point(12, 12)
+        Me.Submit.Name = "Submit"
+        Me.Submit.Size = New System.Drawing.Size(166, 29)
+        Me.Submit.TabIndex = 0
+        Me.Submit.Text = "Submit Entry"
+        Me.Submit.UseVisualStyleBackColor = True
+        '
+        'Clear
+        '
+        Me.Clear.Location = New System.Drawing.Point(12, 47)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(166, 29)
+        Me.Clear.TabIndex = 1
+        Me.Clear.Text = "Clear Entry"
+        Me.Clear.UseVisualStyleBackColor = True
+        '
+        'Entry
+        '
+        Me.Entry.Location = New System.Drawing.Point(12, 82)
+        Me.Entry.Name = "Entry"
+        Me.Entry.Size = New System.Drawing.Size(166, 27)
+        Me.Entry.TabIndex = 2
+        Me.Entry.Text = "Please enter something."
+        '
+        'DisplayEntry
+        '
+        Me.DisplayEntry.Location = New System.Drawing.Point(12, 115)
+        Me.DisplayEntry.Multiline = True
+        Me.DisplayEntry.Name = "DisplayEntry"
+        Me.DisplayEntry.Size = New System.Drawing.Size(120, 60)
+        Me.DisplayEntry.TabIndex = 3
+        Me.DisplayEntry.Visible = False
+        '
+        'Display
+        '
+        Me.Display.Location = New System.Drawing.Point(12, 181)
+        Me.Display.Name = "Display"
+        Me.Display.Size = New System.Drawing.Size(120, 29)
+        Me.Display.TabIndex = 4
+        Me.Display.Text = "Display"
+        Me.Display.UseVisualStyleBackColor = True
+        '
+        'GetEntries
+        '
+        Me.GetEntries.Location = New System.Drawing.Point(12, 250)
+        Me.GetEntries.Name = "GetEntries"
+        Me.GetEntries.Size = New System.Drawing.Size(120, 29)
+        Me.GetEntries.TabIndex = 5
+        Me.GetEntries.Text = "Get Entries"
+        Me.GetEntries.UseVisualStyleBackColor = True
+        '
+        'PickEntries
+        '
+        Me.PickEntries.Enabled = False
+        Me.PickEntries.FormattingEnabled = True
+        Me.PickEntries.Location = New System.Drawing.Point(12, 216)
+        Me.PickEntries.Name = "PickEntries"
+        Me.PickEntries.Size = New System.Drawing.Size(151, 28)
+        Me.PickEntries.TabIndex = 6
+        Me.PickEntries.Text = "Select an Entry"
+        '
+        'DeleteEntry
+        '
+        Me.DeleteEntry.Enabled = False
+        Me.DeleteEntry.Location = New System.Drawing.Point(138, 115)
+        Me.DeleteEntry.Name = "DeleteEntry"
+        Me.DeleteEntry.Size = New System.Drawing.Size(112, 29)
+        Me.DeleteEntry.TabIndex = 7
+        Me.DeleteEntry.Text = "Delete Entry"
+        Me.DeleteEntry.UseVisualStyleBackColor = True
+        '
+        'EditEntry
+        '
+        Me.EditEntry.Enabled = False
+        Me.EditEntry.Location = New System.Drawing.Point(138, 146)
+        Me.EditEntry.Name = "EditEntry"
+        Me.EditEntry.Size = New System.Drawing.Size(112, 29)
+        Me.EditEntry.TabIndex = 8
+        Me.EditEntry.Text = "Edit Entry"
+        Me.EditEntry.UseVisualStyleBackColor = True
+        '
+        'SubmitEdit
+        '
+        Me.SubmitEdit.Enabled = False
+        Me.SubmitEdit.Location = New System.Drawing.Point(256, 146)
+        Me.SubmitEdit.Name = "SubmitEdit"
+        Me.SubmitEdit.Size = New System.Drawing.Size(112, 29)
+        Me.SubmitEdit.TabIndex = 9
+        Me.SubmitEdit.Text = "Submit Edit"
+        Me.SubmitEdit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SubmitEdit)
+        Me.Controls.Add(Me.EditEntry)
+        Me.Controls.Add(Me.DeleteEntry)
+        Me.Controls.Add(Me.PickEntries)
+        Me.Controls.Add(Me.GetEntries)
+        Me.Controls.Add(Me.Display)
+        Me.Controls.Add(Me.DisplayEntry)
+        Me.Controls.Add(Me.Entry)
+        Me.Controls.Add(Me.Clear)
+        Me.Controls.Add(Me.Submit)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Submit As Button
+    Friend WithEvents Clear As Button
+    Friend WithEvents Entry As TextBox
+    Friend WithEvents DisplayEntry As TextBox
+    Friend WithEvents Display As Button
+    Friend WithEvents GetEntries As Button
+    Friend WithEvents PickEntries As ComboBox
+    Friend WithEvents DeleteEntry As Button
+    Friend WithEvents EditEntry As Button
+    Friend WithEvents SubmitEdit As Button
+End Class
---
+++ MyDiary/MyDiary/Form1.vb
@@ -0,0 +1,152 @@
+﻿Imports System.CodeDom
+Imports System.IO
+Imports System.Transactions
+
+Public Class Form1
+    Private Sub Submit_Click(sender As Object, e As EventArgs) Handles Submit.Click
+        Dim fw As StreamWriter
+
+        Dim ReadString As String
+
+        If (Entry.Text = "" Or Entry.Text = "Please enter something.") Then
+            Entry.Text = "Please enter something."
+            Return
+        End If
+        Try
+            ' Pass the file path and name to the StreamWriter constructor.
+            ' Indicate that Append is True, so file will not be overwritten.
+            fw = New StreamWriter("C:\tmp\MyDiary.txt", True)
+            Dim Today As DateTime
+            Today = Now
+            fw.Write(CStr(Today))
+            fw.Write(ControlChars.CrLf)
+            ReadString = Entry.Text
+            fw.WriteLine(ReadString)
+        Finally
+            ' Close the file.
+            fw.Close()
+
+        End Try
+    End Sub
+
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        Entry.Text = ""
+    End Sub
+
+    Private Sub GetEntries_Click(sender As Object, e As EventArgs) Handles GetEntries.Click
+        Dim fr As StreamReader = Nothing
+        Dim FileString As String
+        FileString = ""
+        Try
+            fr = New System.IO.StreamReader("C:\tmp\MyDiary.txt")
+            PickEntries.Items.Clear()
+            PickEntries.Enabled = True
+            Do
+                FileString = fr.ReadLine
+                If IsDate(FileString) Then
+                    PickEntries.Items.Add(FileString)
+                End If
+            Loop Until (FileString Is Nothing)
+        Finally
+            If fr IsNot Nothing Then
+                fr.Close()
+            End If
+        End Try
+        PickEntries.Enabled = True
+    End Sub
+
+    Private Sub Display_Click(sender As Object, e As EventArgs) Handles Display.Click
+        Dim fr As StreamReader
+        Dim ReadString As String
+        ' Make sure ReadString begins empty.
+        ReadString = ""
+        Dim FileString As String
+        fr = New StreamReader("C:\tmp\MyDiary.txt")
+        ' If no entry has been selected, show the whole file.
+        If PickEntries.Enabled = False Or PickEntries.SelectedText Is Nothing Then
+            Do
+                ' Read a line from the file into FileString.
+                FileString = fr.ReadLine
+                ' Add it to ReadString
+                ReadString = ReadString & ControlChars.CrLf & FileString
+            Loop Until (FileString = Nothing)
+        Else
+            ' An entry has been selected, find the line that matches.
+            Do
+
+                FileString = fr.ReadLine
+            Loop Until FileString = CStr(PickEntries.SelectedItem)
+            FileString = CStr(PickEntries.SelectedItem) & ControlChars.CrLf
+            ReadString = ReadString & fr.ReadLine
+
+            ' Read from the file until EOF or another Date is found.
+            Do Until ((fr.Peek < 0) Or (IsDate(fr.ReadLine)))
+                ReadString = ReadString & fr.ReadLine
+            Loop
+        End If
+        fr.Close()
+        DisplayEntry.Visible = True
+        DisplayEntry.Text = ReadString
+        DeleteEntry.Enabled = True
+        EditEntry.Enabled = True
+    End Sub
+
+    Private Sub DeleteEntry_Click(sender As Object, e As EventArgs) Handles DeleteEntry.Click
+        Dim fr As StreamReader
+        Dim ReadString As String
+        Dim WriteString As String
+        Dim ConfirmDelete As MsgBoxResult
+        fr = New StreamReader("C:\tmp\MyDiary.txt")
+        ReadString = fr.ReadLine
+        ' Read through the text file.
+        Do Until (fr.Peek < 0)
+            ReadString = ReadString & vbCrLf & fr.ReadLine
+        Loop
+        WriteString = Replace(ReadString, DisplayEntry.Text, "")
+        fr.Close()
+        ' Check to make sure th euser wishes to delete the entry.
+        ConfirmDelete = MsgBox("Do you really wish to delete this entry?", MsgBoxStyle.OkCancel)
+        If ConfirmDelete = MsgBoxResult.Ok Then
+            File.Delete("C:\tmp\MyDiary.txt")
+            Dim fw As StreamWriter = File.CreateText("C:\tmp\MyDiary.txt")
+            fw.WriteLine(WriteString)
+            fw.Close()
+            ' Reset controls on the form.
+            DisplayEntry.Text = ""
+            PickEntries.Text = ""
+            PickEntries.Items.Clear()
+            PickEntries.Enabled = False
+            DeleteEntry.Enabled = False
+        End If
+    End Sub
+
+    Private Sub EditEntry_Click(sender As Object, e As EventArgs) Handles EditEntry.Click
+        Entry.Text = DisplayEntry.Text
+        SubmitEdit.Enabled = True
+    End Sub
+
+    Private Sub SubmitEdit_Click(sender As Object, e As EventArgs) Handles SubmitEdit.Click
+        Dim fr As StreamReader
+        Dim ReadString As String
+        Dim WriteString As String
+        If Entry.Text = "" Then
+            MsgBox("Use Delete to Delete an Entry")
+            Return
+        End If
+        fr = New StreamReader("C:\tmp\MyDiary.txt")
+        ReadString = fr.ReadLine
+        Do Until (fr.Peek < 0)
+            ReadString = ReadString & vbCrLf & fr.ReadLine
+        Loop
+        WriteString = Replace(ReadString, DisplayEntry.Text, Entry.Text)
+        fr.Close()
+        File.Delete("C:\tmp\MyDiary.txt")
+        Dim fw As StreamWriter = File.CreateText("C:\tmp\MyDiary.txt")
+        fw.WriteLine(WriteString)
+        fw.Close()
+        DisplayEntry.Text = Entry.Text
+        Entry.Text = ""
+        EditEntry.Enabled = False
+        SubmitEdit.Enabled = False
+    End Sub
+End Class
