1f9d96a71aeb5772923f8393f23bf6d028094345
zredcay <60413606+zredcay@users.noreply.github.com>
2022-11-02 12:25:41 -0400

    Add files via upload

---
+++ TypeEquivalenceClient/TypeEquivalenceClient/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Reflection
+Imports TypeEquivalenceInterface
+
+Module Module1
+
+    Sub Main()
+        Dim sampleAssembly = Assembly.Load("TypeEquivalenceRuntime")
+        Dim sampleClass As ISampleInterface = CType(
+            sampleAssembly.CreateInstance("TypeEquivalenceRuntime.SampleClass"), ISampleInterface)
+        sampleClass.GetUserInput()
+        Console.WriteLine(sampleClass.UserInput)
+        Console.WriteLine(sampleAssembly.GetName().Version)
+        Console.ReadLine()
+    End Sub
+
+End Module
