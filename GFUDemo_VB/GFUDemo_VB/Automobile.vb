be748061bf37fd25d05341046d8c54911950881c
zredcay <60413606+zredcay@users.noreply.github.com>
2022-11-08 12:24:10 -0500

    Add files via upload

---
+++ GFUDemo_VB/GFUDemo_VB/Automobile.vb
@@ -0,0 +1,24 @@
+﻿Public Class Automobile
+    Sub New()
+        Model = "Not Specified"
+        TopSpeed = -1
+        IsRunning = True
+    End Sub
+
+    Public Sub New(model As String, topSpeed As Integer)
+        Me.Model = model
+        Me.TopSpeed = topSpeed
+    End Sub
+
+    Public Property Model As String
+    Public Property TopSpeed As Integer
+    Public Property IsRunning As Boolean
+
+    Public Sub Start()
+        If Model <> "Not specified" Or TopSpeed <> -1 Then
+            IsRunning = True
+        Else
+            IsRunning = False
+        End If
+    End Sub
+End Class
