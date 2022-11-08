2c2b1b336400db75de43571f03c45f80bf9a28b1
zredcay <60413606+zredcay@users.noreply.github.com>
2022-11-08 12:24:45 -0500

    Add files via upload

---
+++ GFUDemo_VB/UnitTestProject1/AutomobileTest.vb
@@ -0,0 +1,17 @@
+﻿Imports GFUDemo_VB
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class AutomobileTest
+
+    <TestMethod()> Public Sub DefaultAutomobileIsInitializedCorrectly()
+        Dim myAuto = New Automobile()
+        Assert.IsTrue((myAuto.Model = "Not Specified") And (myAuto.TopSpeed = -1))
+    End Sub
+    <TestMethod()> Public Sub AutomobileWithModelNameCanStart()
+        Dim model As String = "Contoso"
+        Dim topSpeed As Integer = 199
+        Dim myAuto As New Automobile(model, topSpeed)
+        myAuto.Start()
+        Assert.IsTrue(myAuto.IsRunning = True)
+    End Sub
+End Class
---
+++ GFUDemo_VB/UnitTestProject1/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("UnitTestProject1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("UnitTestProject1")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("d1af2886-7932-406b-a0f3-3314be6e8c24")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
